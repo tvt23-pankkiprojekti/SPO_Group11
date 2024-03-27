0c99f67df51d0a5093fb1146f4275235ce53d84b
rauaap <89995622+rauaap@users.noreply.github.com>
2024-03-27 11:45:04 +0000

    Initialize backend part of the project (#19)
    * Initialize backend part of the project
    
    * remove userRouter require from server.js

---
+++ backend/app/src/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+module.exports = {
+    PORT: process.env.PORT,
+    DB_HOST: process.env.DB_HOST,
+    DB_USER: process.env.DB_USER,
+    DB_PASSWORD: process.env.DB_PASSWORD,
+    DB_NAME: process.env.DB_NAME,
+    SECRET: process.env.SECRET
+}
