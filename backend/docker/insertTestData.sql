afb617140c04ac2aa9caba20f45fef70deb2598a
rauaap <89995622+rauaap@users.noreply.github.com>
2024-03-28 13:20:30 +0000

    DB defaults, NN settings and test data (#21)
    * add defaults for Card.frozen and Card.failedPinAttempts and make them NN, add default NULL to Transaction.Card_id
    
    * add auto increment for Administrator primary key
    
    * sql inserts for test data

---
+++ backend/docker/insertTestData.sql
@@ -0,0 +1,71 @@
+INSERT INTO `mydb`.`Administrator` (`login`, `passwordHash`) VALUES 
+('admin1', 'hash1'),
+('admin2', 'hash2'),
+('admin3', 'hash3');
+
+INSERT INTO `mydb`.`User` (`firstName`, `lastName`) 
+VALUES 
+('John', 'Doe'), 
+('Alice', 'Smith'), 
+('Michael', 'Johnson'), 
+('Emily', 'Brown'), 
+('Daniel', 'Williams'),
+('Sarah', 'Taylor'),
+('Christopher', 'Anderson'),
+('Emma', 'Martinez'),
+('David', 'Garcia'),
+('Olivia', 'Miller');
+
+INSERT INTO `mydb`.`Account` (`User_id`, `type`, `balance`, `limit`)
+VALUES 
+(1, 'debit', 100.00, 0.00),
+(2, 'debit', 1500.00, 0.00),
+(3, 'debit', 2000.00, 0.00),
+(4, 'credit', 1200.50, 1300.00),
+(5, 'credit', 1800.00, 2000.00),
+(6, 'debit', 10.00, 0.00),
+(7, 'debit', 1700.00, 0.00),
+(8, 'credit', 1100.00, 1200.00),
+(9, 'debit', 20.90, 0.00),
+(10, 'debit', 1900.00, 0.00);
+
+INSERT INTO `mydb`.`Friend` (`User_id`, `Account_id`)
+VALUES
+(1, 2),
+(7, 10);
+
+INSERT INTO `mydb`.`Card` (`User_id`, `pinHash`, `number`) VALUES 
+(1, 'hash1', '1234567890123456'),
+(2, 'hash2', '2345678901234567'),
+(3, 'hash3', '3456789012345678'),
+(4, 'hash4', '4567890123456789'),
+(5, 'hash5', '5678901234567890'),
+(6, 'hash6', '6789012345678901'),
+(7, 'hash7', '7890123456789012'),
+(8, 'hash8', '8901234567890123'),
+(9, 'hash9', '9012345678901234'),
+(10, 'hash10', '0123456789012345');
+
+INSERT INTO `mydb`.`Card_Account` (`Card_id`, `Account_id`) VALUES 
+(1, 1),
+(2, 2),
+(3, 3),
+(4, 4),
+(5, 5),
+(6, 6),
+(7, 7),
+(8, 8),
+(9, 9),
+(10, 10);
+
+INSERT INTO `mydb`.`Transaction` (`Card_id`, `Account_id`, `dateTime`, `balanceChange`, `transactionType`) VALUES 
+(1, 1, NOW(), -50.00, 'withdraw'),
+(2, 2, NOW(), 100.00, 'deposit'),
+(3, 3, NOW(), -20.00, 'withdraw'),
+(4, 4, NOW(), 200.00, 'deposit'),
+(5, 5, NOW(), -10.00, 'withdraw'),
+(6, 6, NOW(), 50.00, 'deposit'),
+(7, 7, NOW(), -30.00, 'withdraw'),
+(8, 8, NOW(), 300.00, 'deposit'),
+(9, 9, NOW(), -40.00, 'withdraw'),
+(10, 10, NOW(), 400.00, 'deposit');
