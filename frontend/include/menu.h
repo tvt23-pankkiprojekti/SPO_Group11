723264ee49a07491837571c3e89102162d435ec6
unknown <ana.pitkala@gmail.com>
2024-04-19 17:21:16 +0300

    main menu done

--- frontend/include/menu.h
+++ frontend/include/menu.h
@@ -2,6 +2,9 @@
 #define MENU_H
 
 #include <QWidget>
+#include <QTimer>
+
+using namespace std::chrono_literals;
 
 namespace Ui {
 class Menu;
@@ -15,8 +18,23 @@ public:
     explicit Menu(QWidget *parent = nullptr);
     ~Menu();
 
+    static constexpr auto user_action_timeout = 30s;
+    QTimer m_user_action_timer;
+
+signals:
+    void withdraw_selected();
+    void showBalance_selected();
+    void showTransactions_selected();
+    void logOut();
+
 private:
     Ui::Menu *ui;
+
+    void on_buttonWithdraw_clicked();
+    void on_buttonShowBalance_clicked();
+    void on_buttonShowTransactions_clicked();
+    void on_buttonLogout_clicked();
+    void time_run_out();
 };
 
 #endif // MENU_H
