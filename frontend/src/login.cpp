81c8be30be3be8c48b74b452b7b4e4569e693f10
rauaap <aapr@protonmail.com>
2024-04-21 00:00:13 +0300

    login widget; add timeout to status widget

--- frontend/src/login.cpp
+++ frontend/src/login.cpp
@@ -1,14 +1,275 @@
 #include "login.h"
 #include "ui_login.h"
 
-Login::Login(QWidget *parent)
+#ifdef QT_DEBUG
+#include <QDebug>
+#endif
+
+#include "mainwindow.h"
+#include <CardReader/cardreader.h>
+
+Login::Login(REST* const restPtr, QWidget *parent)
     : QWidget(parent)
     , ui(new Ui::Login)
+    , m_reader(CardReader::getInstance())
+    , m_rest(restPtr)
+    , m_mainWindow(parent)
 {
     ui->setupUi(this);
+
+    mainContainer = qobject_cast<QStackedWidget*>(
+        ui->stackedWidget_loginContainer
+    );
+
+    // !------- Connect non-dynamic signals -------!
+    QObject::connect(
+        ui->lineEdit_pin,
+        &QLineEdit::textEdited,
+        this,
+        [this]{
+            #ifdef QT_DEBUG
+            qDebug() << "Pin input edited -> resetting timer";
+            #endif
+            // Reset user action timer every time
+            // pin input is edited
+            userActionTimer.start();
+        }
+    );
+
+    QObject::connect(
+        ui->pushButton_cancelEnterPin,
+        &QPushButton::clicked,
+        this,
+        [this]{
+            reset();
+        }
+    );
+
+    QObject::connect(
+        ui->pushButton_cancelSelectAccunt,
+        &QPushButton::clicked,
+        this,
+        [this]{
+            reset();
+        }
+    );
+
+    QObject::connect(
+        &userActionTimer,
+        &QTimer::timeout,
+        this,
+        [this]{
+            #ifdef QT_DEBUG
+            qDebug() << "User action timeout in login";
+            #endif
+
+            reset();
+        }
+    );
+
+    QObject::connect(
+        ui->pushButton_debit,
+        &QPushButton::clicked,
+        this,
+        [this]{
+            ui->pushButton_debit->setEnabled(false);
+            ui->pushButton_credit->setEnabled(false);
+            emit loggedIn(m_tokens["debit"].toString());
+            reset();
+        }
+    );
+
+    QObject::connect(
+        ui->pushButton_credit,
+        &QPushButton::clicked,
+        this,
+        [this]{
+            ui->pushButton_debit->setEnabled(false);
+            ui->pushButton_credit->setEnabled(false);
+            emit loggedIn(m_tokens["credit"].toString());
+            reset();
+        }
+    );
+    // !-------------------------------------------!
+
+
+    // !----------- Initialize objects ------------!
+    auto port = CardReader::findPort();
+
+    if (!port) {
+        throw std::runtime_error("Card reader not found.");
+    }
+
+    m_reader.open(port);
+    userActionTimer.setSingleShot(true);
+    userActionTimer.setInterval(USER_ACTION_TIMEOUT);
+    // !-------------------------------------------!
+
+    enterInsertCard();
 }
 
 Login::~Login()
 {
     delete ui;
+}
+
+// Some of the signals need to be connected after this widget has been
+// added to the stacked widget in the MainWindow and it has taken
+// ownership of this (Login)
+void Login::connectPostConstructorSignals(void) {
+    auto login = [this]{
+        // User is in status screen -> don't let them interact
+        if (qobject_cast<QStackedWidget*>(parent())->currentWidget() != this) {
+            return;
+        }
+
+        ui->pushButton_login->setEnabled(false);
+        ui->lineEdit_pin->setEnabled(false);
+
+        // User attempts login so we stop the userActionTimer
+        userActionTimer.stop();
+        const auto pin = ui->lineEdit_pin->text();
+
+        #ifdef QT_DEBUG
+        qDebug() << "Login clicked with card number: " << m_cardNumber;
+        #endif
+
+        enterLoggingIn(pin);
+    };
+
+    QObject::connect(ui->pushButton_login, &QPushButton::clicked, this, login);
+    QObject::connect(ui->lineEdit_pin, &QLineEdit::returnPressed, this, login);
+
+    // Used to reset the timer when user closes the status window
+    // The signal is sent by the owning QStackedWidget
+    QObject::connect(
+        qobject_cast<QStackedWidget*>(parent()),
+        &QStackedWidget::currentChanged,
+        this,
+        [this](int currentWidgetIdx){
+            if (
+                mainContainer->currentWidget() == ui->page_enterPin &&
+                currentWidgetIdx == StackedWidgetChildren::LOGIN
+            ) {
+                #ifdef QT_DEBUG
+                qDebug() << "Status exited";
+                #endif
+
+                // User came back from status screen -> re-start timer
+                userActionTimer.start();
+            }
+        }
+    );
+}
+
+void Login::enterInsertCard(void) {
+    mainContainer->setCurrentWidget(ui->page_insertCard);
+
+    QObject::connect(
+        &m_reader,
+        &CardReader::cardRead,
+        this,
+        [this](QString cardNumber){
+            // Card read -> disconnect this signal
+            // until this phase is entered again
+            m_reader.disconnect();
+
+            m_cardNumber = cardNumber;
+            enterInputPin();
+        }
+    );
+
+    #ifdef QT_DEBUG
+    qDebug() << "enterInsert card: " << m_cardNumber << m_tokens;
+    #endif
+}
+
+void Login::enterInputPin(void) {
+    mainContainer->setCurrentWidget(ui->page_enterPin);
+    ui->pushButton_login->setEnabled(true);
+    ui->lineEdit_pin->setEnabled(true);
+    ui->lineEdit_pin->setFocus();
+    userActionTimer.start();
+}
+
+void Login::enterLoggingIn(const QString pin) {
+    // First connect the signal
+    QObject::connect(
+        m_rest,
+        &REST::login_request_finished,
+        this,
+        [this](Response res){
+            // Response received -> disconnect this signal
+            // until this phase is entered again
+            QObject::disconnect(
+                m_rest,
+                &REST::login_request_finished,
+                nullptr,
+                nullptr
+            );
+
+            #ifdef QT_DEBUG
+            qDebug() << "Response code: " << res.code();
+            #endif
+
+            if ( !res.has_data() ) {
+                qobject_cast<MainWindow*>(m_mainWindow)->show_status(
+                    this, "Login failed", false
+                );
+
+                enterInputPin();
+                return;
+            }
+
+            #ifdef QT_DEBUG
+            qDebug() << "Response data:\n" << res.data();
+            #endif
+
+            m_tokens = res.data();
+            QString debitToken = m_tokens["debit"].toString();
+            QString creditToken = m_tokens["credit"].toString();
+
+            // no credit account found -> open debit account
+            if (creditToken.isEmpty()) {
+                emit loggedIn(debitToken);
+                reset();
+            }
+            // no debit account found -> open credit account
+            else if (debitToken.isEmpty()) {
+                emit loggedIn(creditToken);
+                reset();
+            }
+            // both credit and debit accounts found
+            else {
+                enterSelectAccount();
+            }
+        }
+    );
+
+    // Then make the login request
+    m_rest->make_login_request(m_cardNumber, pin);
+}
+
+void Login::enterSelectAccount() {
+    mainContainer->setCurrentWidget(ui->page_selectAccount);
+    userActionTimer.start();
+    ui->pushButton_debit->setEnabled(true);
+    ui->pushButton_credit->setEnabled(true);
+}
+
+void Login::reset(void) {
+    userActionTimer.stop();
+    ui->lineEdit_pin->clear();
+    m_reader.disconnect();
+    m_cardNumber.clear();
+    m_tokens = QJsonDocument();
+
+    QObject::disconnect(
+        m_rest,
+        &REST::login_request_finished,
+        nullptr,
+        nullptr
+    );
+
+    enterInsertCard();
 }
--- frontend/src/mainwindow.cpp
+++ frontend/src/mainwindow.cpp
@@ -9,9 +9,8 @@
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , m_ui(new Ui::MainWindow)
-    , m_reader(CardReader::getInstance())
 
-    , m_login_widget(new Login)
+    , m_login_widget(new Login( REST::the(), this ))
     , m_menu_widget( new Menu(this) )
     , m_balance_widget(new Balance)
     , m_transactions_widget(new Transactions)
@@ -20,6 +19,9 @@ MainWindow::MainWindow(QWidget *parent)
 {
     m_ui->setupUi(this);
 
+    // The order matters here since the QStackedWidget::currentChanged signal
+    // emits the index number of the widget it changed to. It needs to stay
+    // the same as the StackedWidgetChildren enum in mainwindow.h.
     for (auto widget: std::initializer_list<QWidget*>{
         m_login_widget,
         m_menu_widget,
@@ -40,6 +42,22 @@ MainWindow::MainWindow(QWidget *parent)
         }
     });
 
+    m_login_widget->connectPostConstructorSignals();
+
+    QObject::connect(
+        m_login_widget,
+        &Login::loggedIn,
+        this,
+        [this](QString token) {
+            #ifdef QT_DEBUG
+            qDebug() << token;
+            #endif
+
+            m_token = token;
+            m_ui->stackedWidget->setCurrentWidget(m_menu_widget);
+        }
+    );
+
     // m_ui->stackedWidget->setCurrentWidget(m_withdraw_widget);
     // show_status(m_withdraw_widget, ":D", false);
 
@@ -59,82 +77,6 @@ MainWindow::MainWindow(QWidget *parent)
         m_token.clear();
         show_widget(m_login_widget);
     });
-
-    // TODO: Implement these
-
-    // REST::the()->make_login_request("1000200030004000", "1111");
-    // REST::the()->make_login_request("1000200030004001", "2222");
-    // REST::the()->make_login_request("1000200030004002", "3333");
-    // REST::the()->make_login_request("1000200030004003", "4444");
-    // REST::the()->make_login_request("1000200030004004", "5555");
-    
-    // connect(..., ..., this, [this]() {
-    //     REST::make_login_request(...);
-    // });
-
-    // connect(..., ..., this, [this]() {
-    //     REST::make_balance_request(...);
-    // });
-
-    // connect(..., ..., this, [this]() {
-    //     REST::make_prewithdraw_request(...);
-    // });
-
-    // connect(..., ..., this, [this]() {
-    //     REST::make_withdraw_request(...);
-    // });
-
-    // connect(..., ..., this, [this]() {
-    //     REST::make_transactions_request(...);
-    // });
-
-    // connect(REST::the(), &REST::login_request_finished, this, [this](Response response) {
-    //     if (response.has_data()) {
-    //         auto data = response.data().object();
-
-    //         m_token = data["debit"].toString();
-    //         qDebug() << "token: " << m_token;
-    //     }
-    // });
-
-    // connect(REST::the(), &REST::balance_request_finished, this, [this](Response response) {
-    //
-    // });
-
-    // connect(REST::the(), &REST::prewithdraw_request_finished, this, [this](Response response) {
-    //     if (response.has_data()) {
-    //         auto data = response.data().object();
-
-    //         qDebug() << "firstName: " << data["firstName"].toString();
-    //         qDebug() << "lastName: " << data["lastName"].toString();
-    //         qDebug() << "balance: " << data["balance"].toString().toDouble();
-        
-    //         REST::the()->make_withdraw_request(m_token, 100);
-    //     }
-    // });
-
-    // connect(REST::the(), &REST::withdraw_request_finished, this, [this](Response response) {
-    //     if (response.has_data()) {
-    //         auto data = response.data().object();
-
-    //         qDebug() << "amount: " << data["amount"].toDouble();
-        
-    //         REST::the()->make_transactions_request(m_token);
-    //     }
-    // });
-
-    // connect(REST::the(), &REST::transactions_request_finished, this, [](Response response) {
-    //     if (response.has_data()) {
-    //         auto data = response.data().array();
-
-    //         qDebug() << "transactions: ";
-    //         for (auto transaction : data) {
-    //             auto datetime = transaction.toObject()["dateTime"].toString();
-    //             auto change = transaction.toObject()["balanceChange"].toString().toDouble();
-    //             qDebug() << "\tdateTime:" << datetime << ", balanceChange:" << change;
-    //         }
-    //     }
-    // });
 }
 
 MainWindow::~MainWindow()
@@ -148,6 +90,7 @@ void MainWindow::show_status(QWidget * widget, const QString& status, bool visib
     m_status_widget->set_previous_widget(widget);
     m_status_widget->set_status(status);
     m_status_widget->set_menu_widget(visible);
+    m_status_widget->m_user_action_timer.start();
 }
 
 void MainWindow::show_menu()
