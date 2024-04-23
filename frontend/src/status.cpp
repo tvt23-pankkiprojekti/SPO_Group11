60cf301f38cb8b28975232bb81813836f2e81753
rauaap <89995622+rauaap@users.noreply.github.com>
2024-04-23 15:51:53 +0000

    add timeout to status widget (#76)

--- frontend/src/status.cpp
+++ frontend/src/status.cpp
@@ -8,14 +8,32 @@ Status::Status(MainWindow *parent)
 {
     m_ui->setupUi(this);
 
+    m_user_action_timer.setSingleShot(true);
+    m_user_action_timer.setInterval(USER_ACTION_TIMEOUT);
+
+    connect(&m_user_action_timer, &QTimer::timeout, this, [this, parent]{
+        #ifdef QT_DEBUG
+        qDebug() << "User action timeout in status";
+        #endif
+
+        if (m_previous_widget) {
+            parent->show_widget(m_previous_widget);
+        }
+        else {
+            parent->show_menu();
+        }
+    });
+
     /*
      * Buttons
      */
     connect(m_ui->button_previous, &QPushButton::clicked, this, [=, this]() {
         parent->show_widget(m_previous_widget);
+        m_user_action_timer.stop();
     });
     connect(m_ui->button_menu, &QPushButton::clicked, this, [=, this]() {
         parent->show_menu();
+        m_user_action_timer.stop();
     });
 }
 
